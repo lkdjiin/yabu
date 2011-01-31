18ddac1018c3986417a96430b523edf341b5581d
lkdjiin <xavier.nayrac@gmail.com>
2011-01-31 14:54:44 +0100

    standard folder structure

---
+++ lib/yabu.rb
@@ -0,0 +1,14 @@
+require "yabu/options"
+require "yabu/backup"
+require "yabu/backup-deletor"
+require "yabu/version"
+require "yabu/log"
+require "yabu/message"
+require "yabu/copier"
+require "yabu/yabu-config"
+require "yabu/dir-config"
+require "yabu/recovery"
+
+module Yabu
+
+end
